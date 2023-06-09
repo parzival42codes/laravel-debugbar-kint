e03eff84e9a9b20d15232f0ce846dbb5487b023e
Stefan Schlombs <stefan.schlombs@gmail.com>
2023-06-09 13:48:21 +0200

    WIP

--- src/App/Services/KintDumpService.php
+++ src/App/Services/KintDumpService.php
@@ -32,12 +32,8 @@ class KintDumpService
             ->limit(1)
             ->frames();
 
-        $file = '';
-        $line = '';
-        if ($backtrace[0]) {
-            $file = $backtrace[0]->file;
-            $line = $backtrace[0]->lineNumber;
-        }
+        $file = $backtrace[0]->file;
+        $line = $backtrace[0]->lineNumber;
 
         $title = $context['_'] ?? '';
         unset($context['_']);
@@ -152,7 +148,7 @@ class KintDumpService
     {
         foreach (self::$dumpCollection['log'] as $item) {
             Log::channel(config('kint.log'))
-                ->$type($item['content'].PHP_EOL.$item['file'].' # '.$item['line'], $item['context']);
+                ->$type($item['content'] . PHP_EOL . $item['file'] . ' # ' . $item['line'], $item['context']);
         }
 
         return $this;
@@ -164,10 +160,10 @@ class KintDumpService
     private function parseDumpView(array $item): string
     {
         if ($item['title'] || $item['context']) {
-            return '<div>'.$item['content'].'<span style="font-size: smaller;"><details><summary>'.$item['title'].' - Context @ '.$item['file'].' # '.$item['line'].' </summary>'.var_export($item['context'],
-                true).' </details></span></div>';
+            return '<div>' . $item['content'] . '<span style="font-size: smaller;"><details><summary>' . $item['title'] . ' - Context @ ' . $item['file'] . ' # ' . $item['line'] . ' </summary>' . var_export($item['context'],
+                true) . ' </details></span></div>';
         } else {
-            return '<div>'.$item['content'].'<span style="font-size: smaller;">@ '.$item['file'].' # '.$item['line'].'</span></div>';
+            return '<div>' . $item['content'] . '<span style="font-size: smaller;">@ ' . $item['file'] . ' # ' . $item['line'] . '</span></div>';
         }
     }
 }
