586f356bbc4d235d09535eac4de7068d9c6c8ffe
Stefan Schlombs <stefan.schlombs@gmail.com>
2023-08-11 10:07:53 +0200

    Clean Code / Optimisation

--- src/App/Services/KintDumpService.php
+++ src/App/Services/KintDumpService.php
@@ -163,8 +163,10 @@ class KintDumpService
     private function parseDumpView(array $item): string
     {
         if ($item['title'] || $item['context']) {
-            return '<div>'.$item['content'].'<span style="font-size: smaller;"><details><summary>'.$item['title'].' - Context @ '.$item['file'].' # '.$item['line'].' </summary>'.var_export($item['context'],
-                true).' </details></span></div>';
+            return '<div>'.$item['content'].'<span style="font-size: smaller;"><details><summary>'.$item['title'].' - Context @ '.$item['file'].' # '.$item['line'].' </summary>'.var_export(
+                $item['context'],
+                true
+            ).' </details></span></div>';
         }
 
         return '<div>'.$item['content'].'<span style="font-size: smaller;">@ '.$item['file'].' # '.$item['line'].'</span></div>';
